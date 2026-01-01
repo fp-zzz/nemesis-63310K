70ae175737c7b9fc900b2ce8e11e07c27020202e
Francisco Pruneda <fgpruneda@protonmail.com>
2026-01-01 17:30:00 -0600

    photo del

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,6 @@
 #include "pros/apix.h"
 
 
-LV_IMAGE_DECLARE(WIN_20250904_16_05_21_Pro);
-
 /*
 ===========================================================
 VEX V5 Robot Program — PROS + LemLib Template
