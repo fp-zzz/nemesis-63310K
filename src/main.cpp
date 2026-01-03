32130c3c71c4e05c1be1b225d60cfcd27c63647c
shriyansS-code <shriyanssangishetty@gmail.com>
2026-01-02 20:42:13 -0600

    6

--- src/main.cpp
+++ src/main.cpp
@@ -189,7 +189,7 @@ void AutoLeft()
     
     //Get to long goal
     chassis.moveToPoint(-35,20,750,{.forwards = false,.minSpeed = 40});
-    chassis.turnToHeading(180,750);
+    chassis.turnToHeading(-180,750);
     chassis.moveToPoint(-35,50,900,{.minSpeed = 40});
 
 
