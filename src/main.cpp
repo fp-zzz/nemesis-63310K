dcea64857df883f7a7da0430fc05bb4c6b95996a
shriyansS-code <shriyanssangishetty@gmail.com>
2026-01-14 21:35:09 -0600

    n

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ constexpr auto out = pros::E_CONTROLLER_DIGITAL_R2; // Outake balls that are bei
 constexpr auto intop = pros::E_CONTROLLER_DIGITAL_L1; // Outakes balls to score
 constexpr auto outtop = pros::E_CONTROLLER_DIGITAL_R1; // Outake balls that are held; Both motors out
 constexpr auto tounge = pros::E_CONTROLLER_DIGITAL_Y; // Piston to control tongue
-constexpr auto wing = pros::E_CONTROLLER_DIGITAL_B; // Piston to control wing/descore
+constexpr auto wing = pros::E_CONTROLLER_DIGITAL_RIGHT; // Piston to control wing/descore
 
 
 // Left motor group on ports 1, 2, 3 (1 & 3 reversed)
@@ -103,7 +103,7 @@ bool lockW = false;
 lemlib::Drivetrain drivetrain(
     &left_motors,                // Left motor group
     &right_motors,               // Right motor group
-    14,                          // Track width (inches)
+    12,                          // Track width (inches)
     lemlib::Omniwheel::NEW_275,    // Wheel type (4" omni)
     450,                         // Max RPM
     2                           // Drift (measured experimentally)
@@ -264,28 +264,30 @@ void AutoWinPoint()
 void AutoSkills()
 {
     //Sets position on the edge of the parking zone **Learn how the Theta works**(for Shriyans);
-    chassis.setPose(-45,-10,180); 
+    chassis.setPose(-45,10,360); 
 
     //intake
-    chassis.turnToPoint(-45,-41,750);
-    chassis.moveToPoint(-45,-41,2000,{.maxSpeed = 70});
-    chassis.turnToPoint(-64,-47,1000);
+    chassis.turnToPoint(-45,41,750);
+    chassis.moveToPoint(-45,41,2000,{.maxSpeed = 70});
+    chassis.turnToPoint(-64,41,1000);
     clamp.set_value(true);
     intake.move(127);
-     chassis.moveToPoint(-68.5,-47,1750,{.maxSpeed = 91});
+     chassis.moveToPoint(-65,41,1750,{.maxSpeed = 70});
     // chassis.moveToPoint(-68,-47,750,{.forwards = false});
     pros::delay(4000);
 
     //Move to 
-    chassis.moveToPoint(-35,-47,1000,{.forwards = false,.maxSpeed = 70});
-    chassis.turnToPoint(-35,-50,900);
-    chassis.moveToPoint(-35,-50,900,{.maxSpeed = 70});
-    chassis.turnToHeading(270,750);
-    chassis.moveToPoint(35,-50,2000,{.forwards = false,.maxSpeed = 70});
-    chassis.turnToPoint(35,-47,900);
-    chassis.moveToPoint(35,-47,900,{.maxSpeed = 70});
-    chassis.turnToHeading(90,750);
-    chassis.moveToPoint(20,-47,900,{.forwards = false,.maxSpeed = 70});
+    chassis.moveToPoint(-45,41,1000,{.forwards = false,.maxSpeed = 70});
+    chassis.turnToPoint(-20,20,1000);
+    intake.move(127);
+    chassis.moveToPoint(-20,20,2000,{.maxSpeed = 70});
+    chassis.turnToPoint(20,20,1000);
+    chassis.moveToPoint(20,20,2000,{.maxSpeed = 70});
+    chassis.turnToPoint(45,41,1000);
+    chassis.moveToPoint(45,41,2000,{.maxSpeed = 70});
+    chassis.turnToHeading(90,900);
+    chassis.moveToPoint(25,41,2000,{.forwards = false,.maxSpeed = 60});
+    top.move(127) && intake.move(127);
 
     
 
@@ -368,18 +370,18 @@ void opcontrol() {
             clamp.set_value(clampValue);
             lockT = true;
         }
-        else if(!(master.get_digital(tounge) && !lockT))
+        else if(!(master.get_digital(tounge)))
         {
             lockT = false;
         }
 
         //  WING PISTON CONTROL
         if (master.get_digital(wing) && !lockW) {
-            clampValue2 != clampValue2;
+            clampValue2 = !clampValue2;
             clamp2.set_value(clampValue2);
             lockW = true;
         }
-        else if(!(master.get_digital(wing) && !lockW))
+        else if(!(master.get_digital(wing)))
         {
             lockW = false;
         }
