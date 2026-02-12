44c83e84181bf5c99e7e0b287940e453d9231b15
Shriyans <shriyanssangishetty@gmail.com>
2026-02-11 20:03:48 -0600

    new

--- src/main.cpp
+++ src/main.cpp
@@ -246,7 +246,7 @@ void AutoLeft()
     chassis.turnToPoint(-55,47,750,{},false);
     chassis.moveToPoint(-55,47,900,{.maxSpeed = 100},false);
     chassis.turnToPoint(-57,47,750,{},false);
-    chassis.moveToPoint(-57,47,{.maxSpeed = 90},false);
+    //chassis.moveToPoint(-57,47,{.maxSpeed = 90},false);
     //adjust by adding points if needed
     //intake for 500ms
 
@@ -278,7 +278,7 @@ void AutoRight()
     chassis.turnToPoint(-55,-47,750,{},false);
     chassis.moveToPoint(-55,-47,900,{.maxSpeed = 100},false);
     chassis.turnToPoint(-57,-47,750,{},false);
-    chassis.moveToPoint(-57,-47,{.maxSpeed = 90},false);
+    //chassis.moveToPoint(-57,-47,{.maxSpeed = 90},false);
     //adjust by adding points if needed
     //intake for 500ms
 
